48bcfd061b4e2007c79424ac17105eddfb7c9afc
Dmitry Nevada <vegnerdima@yandex.ru>
2021-02-02 04:07:46 +0700

    Enhancement #5: `About`-page restyled, minor fixes

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  ngOnInit(): void { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  downloadWork(work: number): void {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', `../../../reports/work${work}.pdf`);
+    link.setAttribute('download', `work${work}.pdf`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
-
 }
