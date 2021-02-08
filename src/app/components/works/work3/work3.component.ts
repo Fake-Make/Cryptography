2b50756c40058708a9f3eb3dffd2c2c0c056d290
Dmitry Nevada <vegnerdima@yandex.ru>
2021-02-08 23:22:09 +0700

    Feature #7: Third work done
    * `DES` algorithm implemented;
    * `GOST` algorithm implemented as `DES`-class extender;
    * `Avalanche criterias` researching implemented;
    * Separate page with interface developed;

--- src/app/components/works/work3/work3.component.ts
+++ src/app/components/works/work3/work3.component.ts
@@ -1,4 +1,11 @@
+import { Converter } from '@core/converter';
+import { Gamma } from '@core/gamma';
+import { Avalanche } from '@core/avalanche';
+
 import { Component, OnInit } from '@angular/core';
+import { Label } from 'ng2-charts';
+import { GOST } from '@/app/core/gost';
+import { DES } from '@/app/core/des';
 
 @Component({
   selector: 'app-work3',
@@ -6,10 +13,123 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work3.component.css']
 })
 export class Work3Component implements OnInit {
+  checkedGost = true;
 
-  constructor() { }
+  viewModes = ['Str', 'Bin', 'Hex'];
+  views = {base: 0, gamma: 1, cipher: 0};
 
-  ngOnInit(): void {
+  base = 'Hello Darkness my old friend. I\'ve come to talk with you again';
+  gamma = '';
+  cipher = '';
+
+  target: 'base' | 'gamma' = 'base';
+  targetBit = '1';
+
+  researches = '';
+  lineChartData: any[] = [
+    {data: [0], label: 'Количество изменённых бит'},
+  ];
+  lineChartLabels: Label[] = ['0'];
+
+  ngOnInit() {
+    this.setLabels(this.checkedGost ? 32 : 16);
   }
 
+  changeView(key: 'base' | 'gamma' | 'cipher') {
+    this.views[key] = (this.views[key] + 1) % this.viewModes.length;
+    const from = this[key];
+    const to = [
+      Converter.hexToStr.bind(Converter),
+      Converter.strToBin.bind(Converter),
+      Converter.binToHex.bind(Converter)
+    ][this.views[key]](from);
+    this[key] = to;
+  }
+
+  getGamma(method: 'random' | 'scrambler' = 'random'): void {
+    const gammaSize = this.checkedGost ? 256 : 64;
+    this.gamma = {
+      random: Gamma.getSimpleGamma(gammaSize),
+      scrambler: Gamma.getScramblerGamma(gammaSize).gamma
+    }[method];
+  }
+
+  applyCipher(direction: 'direct' | 'reverse'): void {
+    const binBase = this.getBin('base');
+    const binGamma = this.getBin('gamma');
+
+    const cipher = this.checkedGost ? new GOST(binGamma) : new DES(binGamma);
+    const binCipher = cipher.apply(binBase, direction === 'direct');
+    this.cipher = this.fromBin('cipher', binCipher);
+  }
+
+  updatePlots(): void {
+    // Avalances A
+    let binGamma = this.getBin('gamma');
+    let binBase = this.getBin('base');
+    const cipherA = this.checkedGost ? new GOST(binGamma) : new DES(binGamma);
+    cipherA.apply(binBase);
+    const avalanchesA = cipherA.getAvalanche();
+
+    // Avalances B
+    const changeBit = (bits: string, bit: number): string => {
+      return bits.slice(0, bit) + +!+bits.slice(bit, bit + 1) + bits.slice(bit + 1);
+    }
+
+    if (this.target === 'base')
+      binBase = changeBit(binBase, +this.targetBit - 1);
+    else
+      binGamma = changeBit(binGamma, +this.targetBit - 1);
+
+    const cipherB = this.checkedGost ? new GOST(binGamma) : new DES(binGamma);
+    cipherB.apply(binBase);
+    const avalanchesB = cipherB.getAvalanche();
+
+    // Plots
+    this.setLabels(this.checkedGost ? 32 : 16);
+    this.lineChartData = [{
+      data: Avalanche.getDifferences(avalanchesA, avalanchesB, this.checkedGost ? 32 : 16),
+      label: this.checkedGost ? 'ГОСТ' : 'DES'
+    }];
+  }
+
+  updateResearches() {
+    const binBase = this.getBin('base');
+    const binGamma = this.getBin('gamma');
+
+    const Alg = this.checkedGost ? GOST : DES;
+    const cipher = (binBase: string, binGamma: string): string => {
+      const alg = new Alg(binGamma);
+      return alg.apply(binBase);
+    };
+
+    const researches = Avalanche.getCriterias(binBase, binGamma, cipher, 'key');
+    this.researches = `Среднее число битов выхода, изменяющихся ` +
+      `при изменении одного бита входного вектора: ${Math.round(researches.averageBits)};\n` +
+      `Степень полноты преобразования: ${(100 * researches.conversionCompleteness).toFixed(2)}%;\n` +
+      `Степень лавинного эффекта: ${(100 * researches.avalancheDegree).toFixed(2)}%;\n` + 
+      `Степень соответствия строгому лавинному критерию: ${(100 * researches.strictAvalancheDegree).toFixed(2)}%.`;
+  }
+
+  private setLabels(iterations: 16 | 32 = 16) {
+    this.lineChartLabels = ['Начало', ...'0'.repeat(iterations).split('')
+      .map((_, index) => (index + 1).toString()), 'Конец'];
+  }
+
+  private getBin(key: 'base' | 'gamma' | 'cipher'): string {
+    const from = this[key];
+    return [
+      Converter.strToBin.bind(Converter),
+      (from: string) => from,
+      Converter.hexToBin.bind(Converter)
+    ][this.views[key]](from);
+  }
+
+  private fromBin(key: 'base' | 'gamma' | 'cipher', bin: string): string {
+    return [
+      Converter.binToStr.bind(Converter),
+      (bin: string) => bin,
+      Converter.binToHex.bind(Converter)
+    ][this.views[key]](bin);
+  }
 }
--- src/app/core/avalanche.ts
+++ src/app/core/avalanche.ts
@@ -1,11 +1,9 @@
 import { Gamma } from '@core/gamma';
 
 export class Avalanche {
-	private static FEISTEL_ITERATIONS = 16;
-
-	static getDifferences(setsA: string[], setsB: string[]): number[] {
+	static getDifferences(setsA: string[], setsB: string[], iterations = 16): number[] {
 		const differenceBits = [];
-		for (let i = 0; i < this.FEISTEL_ITERATIONS + 2; i++) {
+		for (let i = 0; i < iterations + 2; i++) {
 			differenceBits.push(
 				Gamma.apply(setsA[i], setsB[i])
 					.split('')
@@ -15,5 +13,108 @@ export class Avalanche {
 		}
 
 		return differenceBits;
+	}
+
+	static getCriterias(strBits: string, key: string, cipher: any, target: 'key' | 'text' = 'text') {
+		const blockLength = 64;
+		strBits = strBits.length % blockLength ?
+			'0'.repeat(strBits.length % blockLength) + strBits :
+			strBits;
+		const fullLength = strBits.length;
+		const N = Math.round(fullLength / blockLength); // #U
+		const n = {
+			key: key,
+			text: strBits
+		}[target].length, m = blockLength;
+
+		const blocks = (strBits.match(/[01]{1,64}/g) || []);
+
+		let dependencyMatrix: number[][] = [];
+		let distanceMatrix: number[][] = [];
+
+		for (let i = 0; i < n; i++) {
+			let dependencyRow: number[] = [];
+			let distanceRow: number[] = [];
+
+			let A = blocks.map(block => block.split('').map(bit => +bit));
+			let B = A.slice().map(block => {
+				const blockCopy = block.slice();
+				if (i < blockCopy.length && target === 'text') {
+					blockCopy[i] = +!blockCopy[i];
+				}
+				return blockCopy;
+			});
+			const keyA = key;
+			const keyB = target !== 'key' ? key :
+				key.slice(0, i) + +!+key.slice(i, i + 1) + key.slice(i + 1);
+			A = A.map(block => cipher(block.join(''), keyA).split('').map((bit: string) => +bit));
+			B = B.map(block => cipher(block.join(''), keyB).split('').map((bit: string) => +bit));
+
+			for (let j = 0; j < m; j++) {
+				// Количество блоков, где при изменении i меняется j
+				dependencyRow[j] = A
+					.map((block, index) => block[j] ^ B[index][j])
+					.reduce((acc, cur) => acc + cur);
+
+				// Количество блоков, в которых число отличных бит == j
+				distanceRow[j] = A.map((block, i) => block
+						.map((bit, j) => bit ^ B[i][j])
+						.reduce((acc, cur) => acc + cur)
+					).filter(diffs => diffs === j + 1)
+					.length;
+			}
+			dependencyMatrix[i] = dependencyRow;
+			distanceMatrix[i] = distanceRow;
+		}
+
+		// Среднее число битов выхода, изменяющихся
+		// при изменении одного бита входного вектора
+		const averageBits = (distanceMatrix: number[][]): number => {
+			return distanceMatrix
+				.map((row: number[]): number => row
+					.map((b, j) => (j + 1) * b)
+					.reduce((acc, cur) => acc + cur)
+				).reduce((acc, cur) => acc + cur) /
+				(distanceMatrix.length * N);
+		}
+
+		// Степень полноты преобразования
+		const conversionCompleteness = (dependencyMatrix: number[][]): number => {
+			return 1 - dependencyMatrix
+				.map(row => row.filter(a => a === 0).length)
+				.reduce((acc, cur) => acc + cur) /
+				(dependencyMatrix.length * dependencyMatrix[0].length);
+		};
+
+		// Степень лавинного эффекта
+		const avalancheDegree = (distanceMatrix: number[][]): number => {
+			const n = distanceMatrix.length;
+			const m = distanceMatrix[0].length;
+
+			return 1 - distanceMatrix
+				.map(row => Math.abs(
+					row.map((b, j) => 2 * (j + 1) * b)
+						.reduce((acc, cur) => acc + cur) / N  - m
+				)).reduce((acc, cur) => acc + cur) /
+				(n * m);
+		};
+
+		// Степень соответствия строгому лавинному критерию
+		const strictAvalancheDegree = (dependencyMatrix: number[][]): number => {
+			return 1 - dependencyMatrix
+			.map(row => row
+				.map(a => Math.abs(2 * a / N - 1))
+				.reduce((acc, cur) => acc + cur)
+			)
+			.reduce((acc, cur) => acc + cur) /
+			(dependencyMatrix.length * dependencyMatrix[0].length);
+		};
+
+		return {
+			averageBits: averageBits(distanceMatrix),
+			conversionCompleteness: conversionCompleteness(dependencyMatrix),
+			avalancheDegree: avalancheDegree(distanceMatrix),
+			strictAvalancheDegree: strictAvalancheDegree(dependencyMatrix)
+		};
 	}
 }
